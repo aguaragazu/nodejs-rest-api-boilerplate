9fec0aed3db3f328f9ad473be657f40c92567e3f
aguara_gazu <aguara_gazu@yahoo.com.ar>
2021-01-27 00:10:57 -0300

    add index.js in utils and middlewares

---
+++ src/core/middlewares/index.js
@@ -0,0 +1,4 @@
+module.exports.auth = require('./auth');
+module.exports.error = require('./error');
+module.exports.validate = require('./validate');
+module.exports.rateLimiter = require('./rateLimiter');
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+module.exports.catchAsync = require('./catchAsync');
+module.exports.ApiError = require('./ApiError');
+module.exports.pick = require('./pick');
